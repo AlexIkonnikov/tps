2d41567be7d2c1888806d55205ea66f5c33d1be8
Aleksandr <ikonnikovAI@lad24.ru>
2023-03-11 20:56:02 +0600

    re-solve several challenges

--- easy/268-if.ts
+++ easy/268-if.ts
@@ -2,26 +2,24 @@
   268 - If
   -------
   by Pavel Glushkov (@pashutk) #easy #utils
-  
+
   ### Question
-  
-  Implement a utils `If` which accepts condition `C`, a truthy return type `T`, and a falsy return type `F`. `C` is expected to be either `true` or `false` while `T` and `F` can be any type.
-  
+
+  Implement the util type `If<C, T, F>` which accepts condition `C`, a truthy value `T`, and a falsy value `F`. `C` is expected to be either `true` or `false` while `T` and `F` can be any type.
+
   For example:
-  
+
   ```ts
   type A = If<true, 'a', 'b'>  // expected to be 'a'
   type B = If<false, 'a', 'b'> // expected to be 'b'
   ```
-  
+
   > View on GitHub: https://tsch.js.org/268
 */
 
-
 /* _____________ Your Code Here _____________ */
 
-type If<C extends Boolean, T, F> =  C extends true ? T : F
-
+type If<C extends Boolean, T, F> = C extends true ? T : F
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -34,12 +32,9 @@ type cases = [
 // @ts-expect-error
 type error = If<null, 'a', 'b'>
 
-
-
 /* _____________ Further Steps _____________ */
 /*
   > Share your solutions: https://tsch.js.org/268/answer
   > View solutions: https://tsch.js.org/268/solutions
   > More Challenges: https://tsch.js.org
 */
-
--- easy/4-pick.ts
+++ easy/4-pick.ts
@@ -2,33 +2,32 @@
   4 - Pick
   -------
   by Anthony Fu (@antfu) #easy #union #built-in
-  
+
   ### Question
-  
+
   Implement the built-in `Pick<T, K>` generic without using it.
-  
+
   Constructs a type by picking the set of properties `K` from `T`
-  
+
   For example:
-  
+
   ```ts
   interface Todo {
     title: string
     description: string
     completed: boolean
   }
-  
+
   type TodoPreview = MyPick<Todo, 'title' | 'completed'>
-  
+
   const todo: TodoPreview = {
       title: 'Clean room',
       completed: false,
   }
   ```
-  
+
   > View on GitHub: https://tsch.js.org/4
 */
-
 
 /* _____________ Your Code Here _____________ */
 
@@ -36,10 +35,9 @@ type MyPick<T, K extends keyof T> = {
   [key in K]: T[key]
 }
 
-
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
-type Test = MyPick<Todo, 'title' | 'completed'>
+
 type cases = [
   Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
   Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
@@ -61,3 +59,10 @@ interface Expected2 {
   title: string
   completed: boolean
 }
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4/answer
+  > View solutions: https://tsch.js.org/4/solutions
+  > More Challenges: https://tsch.js.org
+*/
